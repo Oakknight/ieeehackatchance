d8d303e87edf573657594fef3eb9279d0a27825b
irunurun <46622874+irunurun@users.noreply.github.com>
2019-01-21 09:55:01 +0300

    Add files via upload

--- webapp.py
+++ webapp.py
@@ -8,6 +8,11 @@ def olurmu(sinirlama):
         return False
     return True
 
+def hasta_isim_al(tckn):
+    hasta_id=devamidb.TCdenID(tckn)
+    hasta_bilgi=devamidb.hastaBilgi(hasta_id)
+    return hasta_bilgi[0]
+
 app = Flask(__name__,static_url_path='/static')
 
 @app.route('/', methods=['GET','POST'])
@@ -16,16 +21,16 @@ def main():
         ilac=request.form["ilac"]
         tckn=request.form["tckn"]
         return redirect("/sorgu/{0}/{1}".format(tckn,ilac))
-    return render_template("girdi.html")
+    return render_template("girdi.html",home=url_for('main'))
 
 @app.route('/sorgu/<tckn>/<ilac>',methods=["GET"])
 def result(tckn,ilac):
     sinirlama=devamidb.ilacKontrol(tckn,ilac)
     risksiz=olurmu(sinirlama)
     if risksiz:
-        return render_template("sonuckotu.html",sinirlama=sinirlama)
+        return render_template("sonuciyi.html",hasta_isim=hasta_isim_al(tckn),ilac_isim=ilac)
     else:
-        return render_template("sonuciyi.html")
+        return render_template("sonuckotu.html",sinirlama=sinirlama,hasta_isim=hasta_isim_al(tckn),ilac_isim=ilac)
 
 @app.route('/ilac/<ilac>',methods=['GET'])
 def ilac_goster(ilac):
